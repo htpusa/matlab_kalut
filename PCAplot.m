5a5e918a6dd3c8940931c5a51ed5c526e9a48b38
htpusa <taneli.pusa@pm.me>
2022-04-08 10:32:07 +0300

    .

--- PCAplot.m
+++ PCAplot.m
@@ -19,7 +19,7 @@ function PCAplot(X, labels, varargin)
     [~,score,~,~,explained] = pca(X);
     comps = reshape(1:8,2,4)';
     figure
-    for i=1:floor(numel(explained)/2)
+    for i=1:min(4,floor(numel(explained)/2))
         subplot(2,2,i);
         gscatter(score(:, comps(i,1)), score(:, comps(i,2)), labels, ...
             lines(numel(unique(labels))))
